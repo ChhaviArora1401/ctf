b01561861ce4ea13902f627f432b8fb10a967dc9
Chhavi Arora <chhaviarora1401@gmail.com>
2021-11-02 23:54:15 +0530

    .

--- index.php
+++ index.php
@@ -4,8 +4,6 @@ require_once __DIR__ . "/login.php";
 <div class="container">		
 		<div class="collapse navbar-collapse" id="navbar1">
 			<ul class="nav navbar-nav navbar-left">
-				<!-- <li><a href="login.php">Login</a></li> -->
-				<!-- <li><a href="register.php">Sign Up</a></li> -->
 			</ul>
 		</div>		
 </div>	
